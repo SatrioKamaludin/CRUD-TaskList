2caafaa8e4b83986aa7f34bebbda62cc6b6287d0
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-16 06:05:07 +0700

    Project setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Button from './component/Button';
+import FormInput from './component/FormInput';
+import TodoItem from './component/TodoItem';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Logo'>
+        <img src={logo} alt='logo'/>
+        <h3>Task List</h3>
+      </div>
+      <div className='list'>
+        <TodoItem />
+      </div>
+      <div className='input-form'>
+        <FormInput />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/FormInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FormInput extends Component {
+    render(){
+        return(
+            <div>
+                Form Input
+            </div>
+        )
+    }
+}
+
+export default FormInput;
